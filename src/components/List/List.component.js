5149c8da8496a9ac2811e781d70f66b052d47e93
Hector DelaRosa <hdelaros@gmail.com>
2022-01-19 08:04:45 -0500

    refactored Links

--- src/components/List/List.component.js
+++ src/components/List/List.component.js
@@ -1,16 +1,22 @@
 import React from 'react';
 
 const List = ({ pictures }) => {
-  return (
-    <div>
-      {pictures.map((picture) => (
-        <div>
-          <img height="150px" width="150px" src={picture.img_src} alt="" />
-          <span>Heart</span>
-        </div>
-      ))}
-    </div>
-  );
+  let list;
+  if (!pictures) {
+    list = <span>No pictures</span>;
+  } else {
+    list = (
+      <div>
+        {pictures.map((picture) => (
+          <div>
+            <img height="150px" width="150px" src={picture.img_src} alt="" />
+            <span>Heart</span>
+          </div>
+        ))}
+      </div>
+    );
+  }
+  return list;
 };
 
 export default List;
